93e0a86da7571e4e5b7de5b392fe9ca922ae44f9
Matt Cadorette <mcadorette@gmail.com>
2018-11-06 17:17:06 -0800

    Initial commit from Atomist

---
+++ src/main/java/qcon876/Qcon876Application.java
@@ -0,0 +1,12 @@
+package qcon876;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Qcon876Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Qcon876Application.class, args);
+	}
+}
